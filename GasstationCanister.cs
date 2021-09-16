365088dc8f3d198f6cdf24c75b35794553cbd050
JunkBeat <61349612+JunkBeat@users.noreply.github.com>
2021-09-16 23:08:59 +0200

    -

--- GasstationCanister.cs
+++ GasstationCanister.cs
@@ -2,7 +2,7 @@
 // Type: GasstationCanister
 // Assembly: Assembly-CSharp, Version=9.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: 0AB8CA89-B4E8-42A6-A741-F039C11ACBC5
-// Assembly location: C:\Users\err00\Desktop\Don't Escape 4 Days in a Wasteland\dontescape_Data\Managed\Assembly-CSharp.dll
+
 
 using UnityEngine;
 
--- TimelineFunction.cs
+++ TimelineFunction.cs
@@ -2,6 +2,6 @@
 // Type: TimelineFunction
 // Assembly: Assembly-CSharp, Version=9.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: 0AB8CA89-B4E8-42A6-A741-F039C11ACBC5
-// Assembly location: C:\Users\err00\Desktop\Don't Escape 4 Days in a Wasteland\dontescape_Data\Managed\Assembly-CSharp.dll
+
 
 public delegate void TimelineFunction(string param);
