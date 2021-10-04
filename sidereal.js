f1ed581f850fa1a4d68e5468644944616e4a5349
JunkBeat <61349612+JunkBeat@users.noreply.github.com>
2021-10-04 14:32:27 +0200

    Update sidereal.js

--- sidereal.js
+++ sidereal.js
@@ -14,6 +14,6 @@
 		p = hex_md5(p);
 		
 		if (l == "a384b6463fc216a5f8ecb6670f86456a" & p == "e00cf25ad42683b3df678c61f42c6bda") {
-			location.href = "https://junkbeat.github.io/Closed/u/user.html";
+			document.getElementById(link).click();
 		}
 	}
